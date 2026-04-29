4f5e716197f9dc96fd087610efffeb66e91a774b
Kaushik Lankoji <kaushiklankoji@Kaushiks-MacBook-Pro.local>
2026-04-28 19:22:36 -0700

    New revamp

--- madlibs/v1/script.js
+++ madlibs/v1/script.js
@@ -16,10 +16,17 @@
         const animal = document.querySelector('#animal').value;
 
         document.querySelector('#out-adj').innerHTML = adj;
+        document.querySelector('#out-adj2').innerHTML = adj;
         document.querySelector('#out-noun').innerHTML = noun;
+        document.querySelector('#out-noun2').innerHTML = noun;
         document.querySelector('#out-verb').innerHTML = verb;
+        document.querySelector('#out-verb2').innerHTML = verb;
         document.querySelector('#out-place').innerHTML = place;
+        document.querySelector('#out-place2').innerHTML = place;
         document.querySelector('#out-animal').innerHTML = animal;
+        document.querySelector('#out-animal2').innerHTML = animal;
+        document.querySelector('#out-animal3').innerHTML = animal;
+        document.querySelector('#out-animal4').innerHTML = animal;
 
         overlay.classList.remove('off');
         overlay.classList.add('on');
